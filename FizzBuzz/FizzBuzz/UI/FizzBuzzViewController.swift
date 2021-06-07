3e99485c1abb6c73276f5f5e775c8cf138e6bac9
Jonathan Seguin <jonathan.seguin@fabernovel.com>
2021-06-07 18:38:43 +0200

    Improve UI with scrollview

--- FizzBuzz/FizzBuzz/UI/FizzBuzzViewController.swift
+++ FizzBuzz/FizzBuzz/UI/FizzBuzzViewController.swift
@@ -40,42 +40,40 @@ class FizzBuzzViewController: UIViewController, FizzBuzzViewContract {
 
     private func setupUI() {
         view.backgroundColor = .white
-        let stackView = UIStackView()
-        stackView.axis = .vertical
-        stackView.alignment = .fill
-        stackView.distribution = .fillEqually
-        stackView.spacing = Constants.defaultSpacing
-        view.addSubview(stackView)
+        let stackView = createStackView()
+        let scrollView = UIScrollView()
+        scrollView.addSubview(stackView)
+        view.addSubview(scrollView)
 
-        [fizzNumberField, buzzNumberField, limitNumberField, fizzTextField, buzzTextField].forEach {
+        [fizzNumberField, buzzNumberField, limitNumberField, fizzTextField, buzzTextField, resultLabel].forEach {
             stackView.addArrangedSubview($0)
         }
+
+        scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        scrollView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        scrollView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+
         stackView.setCustomSpacing(Constants.defaultSpacing*2, after: limitNumberField)
+        stackView.setCustomSpacing(Constants.defaultSpacing*2, after: buzzTextField)
         stackView.topAnchor
-            .constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: Constants.defaultMargin)
+            .constraint(equalTo: scrollView.topAnchor, constant: Constants.defaultMargin)
             .isActive = true
         stackView.leftAnchor
-            .constraint(equalTo: view.leftAnchor, constant: Constants.defaultMargin)
+            .constraint(equalTo: scrollView.leftAnchor, constant: Constants.defaultMargin)
             .isActive = true
         stackView.rightAnchor
-            .constraint(equalTo: view.rightAnchor, constant: -Constants.defaultMargin)
+            .constraint(equalTo: scrollView.rightAnchor, constant: -Constants.defaultMargin)
+            .isActive = true
+        stackView.bottomAnchor
+            .constraint(equalTo: scrollView.bottomAnchor, constant: -Constants.defaultMargin)
             .isActive = true
         stackView.translatesAutoresizingMaskIntoConstraints = false
 
-        view.addSubview(resultLabel)
-        resultLabel.topAnchor
-            .constraint(equalTo: stackView.bottomAnchor, constant: Constants.defaultMargin)
+        stackView.widthAnchor
+            .constraint(equalTo: scrollView.widthAnchor, constant: -2*Constants.defaultMargin)
             .isActive = true
-        resultLabel.leftAnchor
-            .constraint(equalTo: view.leftAnchor, constant: Constants.defaultMargin)
-            .isActive = true
-        resultLabel.rightAnchor
-            .constraint(equalTo: view.rightAnchor, constant: -Constants.defaultMargin)
-            .isActive = true
-        resultLabel.bottomAnchor
-            .constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -Constants.defaultMargin)
-            .isActive = true
-        resultLabel.translatesAutoresizingMaskIntoConstraints = false
     }
 
     private func createNumberField(placeholder: String) -> UITextField {
@@ -107,6 +105,15 @@ class FizzBuzzViewController: UIViewController, FizzBuzzViewContract {
         label.numberOfLines = 0
         label.textColor = .black
         return label
+    }
+
+    private func createStackView() -> UIStackView {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        stackView.spacing = Constants.defaultSpacing
+        return stackView
     }
 
     @objc private func textDidChange(_ textField: UITextField) {
